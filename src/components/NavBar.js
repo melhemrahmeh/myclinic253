5ce48334ed8e03c184c0b570b69ad172d3584e61
Melhem Rahmeh <melhem.rahmehh@gmail.com>
2022-03-01 15:16:41 +0200

    9th commit

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -21,41 +21,54 @@ export default function NavBar(props) {
     operations = "../operations"
 
   }
-  
+
   return (
 
     <div className="container-fluid sticky-top bg-white shadow-sm">
       <div className="container">
         <nav className="navbar navbar-expand-lg bg-white navbar-light py-3 py-lg-0">
-
           <a className="navbar-brand">
             <h1 className="m-0 text-uppercase text-primary">
               <Link to={"../"}> MyClinic</Link></h1>
           </a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
-            <span className="navbar-toggler-icon" />
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse" id="navbarCollapse">
+          <div className="collapse navbar-collapse" id="navbarNav">
             <div className="navbar-nav ms-auto py-0">
-              <a className="nav-item nav-link active">
-                <Link to={"../"}> Home</Link>
-              </a>
-              <a className="nav-item nav-link">
-                <Link to={operations}> Operations</Link>
-              </a>
+              <ul class="navbar-nav">
+                <li class="nav-item active">
+                  <a className="nav-item nav-link active">
+                    <Link to={"../"}> Home</Link>
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a className="nav-item nav-link">
+                    <Link to={operations}> Operations</Link>
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a className="nav-item nav-link">
+                    <Link to={team}>Team</Link>
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a className="nav-item nav-link">
+                    <Link to={about}> About Us</Link>
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a className="nav-item nav-link">
+                    <Link to={login}> Log in</Link>
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a className="nav-item nav-link">
+                    <Link to={join}> Join</Link>
+                  </a>
+                </li>
 
-              <a className="nav-item nav-link">
-                <Link to={team}>Team</Link>
-              </a>
-              <a className="nav-item nav-link">
-                <Link to={about}> About Us</Link>
-              </a>
-              <a className="nav-item nav-link">
-                <Link to={login}> Log in</Link>
-              </a>
-              <a className="nav-item nav-link">
-                <Link to={join}> Join</Link>
-              </a>
+              </ul>
             </div>
           </div>
         </nav>
