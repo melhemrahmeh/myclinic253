1b220cdcdf3324fcb792b517446bd01a69cc8ac9
Melhem Rahmeh <melhem.rahmehh@gmail.com>
2022-03-19 19:02:04 +0200

    Week7 Commit

---
+++ build/precache-manifest.7852586ae0a4967b7a10f03b2891aced.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "861f7e6469d353baffcd",
+    "url": "/static/css/main.1aff3aaa.chunk.css"
+  },
+  {
+    "revision": "861f7e6469d353baffcd",
+    "url": "/static/js/main.39a7cc3e.chunk.js"
+  },
+  {
+    "revision": "5f08022ac81bf9f9cc11",
+    "url": "/static/js/runtime~main.1ec634f5.js"
+  },
+  {
+    "revision": "67c6a2adc172dc0bc7a0",
+    "url": "/static/js/2.10b78bbd.chunk.js"
+  },
+  {
+    "revision": "ec0da083c3153ec63de3",
+    "url": "/static/js/3.3f5239a8.chunk.js"
+  },
+  {
+    "revision": "e515494d783096f31ad6",
+    "url": "/static/js/4.d90fdaf4.chunk.js"
+  },
+  {
+    "revision": "25f0079b53020f6337ac",
+    "url": "/static/js/5.80c3c8ec.chunk.js"
+  },
+  {
+    "revision": "cdfb8b42c61f76ba9331f9371ec3c7f5",
+    "url": "/index.html"
+  }
+];
