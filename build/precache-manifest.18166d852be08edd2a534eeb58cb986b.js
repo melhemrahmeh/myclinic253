d197c3ab04e5e326e1ccbe1aab453c6ee7b5657e
Melhem Rahmeh <melhem.rahmehh@gmail.com>
2022-04-30 04:18:49 +0300

    Commit added

---
+++ build/precache-manifest.18166d852be08edd2a534eeb58cb986b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "554e9ec591a6b3a51f8f",
+    "url": "/static/css/main.f9fce996.chunk.css"
+  },
+  {
+    "revision": "554e9ec591a6b3a51f8f",
+    "url": "/static/js/main.a3d14c64.chunk.js"
+  },
+  {
+    "revision": "1496d8eb877fb9896979",
+    "url": "/static/js/runtime~main.cded5162.js"
+  },
+  {
+    "revision": "67817d9e9474d7f2669d",
+    "url": "/static/css/2.8bec4230.chunk.css"
+  },
+  {
+    "revision": "67817d9e9474d7f2669d",
+    "url": "/static/js/2.7c235426.chunk.js"
+  },
+  {
+    "revision": "4573f2d96c29e7df1a03",
+    "url": "/static/js/3.9bd9c4ca.chunk.js"
+  },
+  {
+    "revision": "e3a807f38263bb6e8cbb",
+    "url": "/static/js/4.d5eb03e0.chunk.js"
+  },
+  {
+    "revision": "652570ad6531882497cb",
+    "url": "/static/js/5.fe757a78.chunk.js"
+  },
+  {
+    "revision": "f6379652c1e3dc5c271a652e66d4044a",
+    "url": "/static/media/logo2.f6379652.png"
+  },
+  {
+    "revision": "d05930b3d689f592185bfcc75abe6026",
+    "url": "/index.html"
+  }
+];
